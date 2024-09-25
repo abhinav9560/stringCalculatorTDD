a8b04ed1b20ecf1674dc4aadf92d7163db05b806
abhinav9560 <abhinavanand0411@gmail.com>
2024-09-25 23:38:48 +0530

    test case for empty string

---
+++ stringCalcutator.test.js
@@ -0,0 +1,8 @@
+// stringCalculator.test.js
+const add = require('./index');
+
+describe('String Calculator', () => {
+    test('should return 0 for an empty string', () => {
+        expect(add("")).toBe(0);
+    });
+});
