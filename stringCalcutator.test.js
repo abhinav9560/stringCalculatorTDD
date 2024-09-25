bbd530552e76172d225c93470825b12c014e3331
abhinav9560 <abhinavanand0411@gmail.com>
2024-09-25 23:40:27 +0530

    test case for only number provided

--- stringCalcutator.test.js
+++ stringCalcutator.test.js
@@ -5,4 +5,10 @@ describe('String Calculator', () => {
     test('should return 0 for an empty string', () => {
         expect(add("")).toBe(0);
     });
+
+    test('should return the number itself if only one number is provided', () => {
+        expect(add("1")).toBe(1);
+        expect(add("5")).toBe(5);
+    });
+
 });
