f38701d85c6910a1bf1bdd69ed105b4845bd52e7
abhinav9560 <abhinavanand0411@gmail.com>
2024-09-25 23:52:48 +0530

    test case to handle any amout of number and  new line

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
-    const delimiter = /,/;
+    const delimiter = /,|\n/;
     const numberArray = numbers.split(delimiter).map(num => parseInt(num));
 
-
+    const filteredNumbers = numberArray.filter(num => num <= 1000);
 
     return filteredNumbers.reduce((sum, num) => sum + num, 0);
 }
--- stringCalcutator.test.js
+++ stringCalcutator.test.js
@@ -16,4 +16,14 @@ describe('String Calculator', () => {
         expect(add("5,7")).toBe(12);
     });
 
+    test('should handle multiple numbers separated by commas', () => {
+        expect(add("1,2,3")).toBe(6);
+        expect(add("4,5,6,7")).toBe(22);
+    });
+
+    test('should handle numbers separated by newline characters', () => {
+        expect(add("1\n2")).toBe(3);
+        expect(add("3\n4\n5")).toBe(12);
+    });
+
 });
